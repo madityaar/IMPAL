9f78f921d606e58db19960b19ec2aad420ed7f0c
Adit <m.adityarayhan@yahoo.co.id>
2018-12-05 18:53:51 +0700

    mindahin  database ke folder

--- impalweb/Database/impal.sql
+++ impalweb/Database/impal.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 11, 2018 at 05:12 PM
+-- Generation Time: Dec 04, 2018 at 06:03 PM
 -- Server version: 10.1.28-MariaDB
 -- PHP Version: 7.1.11
 
@@ -35,6 +35,13 @@ CREATE TABLE `delivery` (
   `tgl_kirim` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `delivery`
+--
+
+INSERT INTO `delivery` (`idDelivery`, `no_resi`, `kurir`, `tgl_kirim`) VALUES
+(1, '1234', 'JNE', '04/dec/2018');
+
 -- --------------------------------------------------------
 
 --
@@ -42,11 +49,74 @@ CREATE TABLE `delivery` (
 --
 
 CREATE TABLE `detail` (
-  `idTransaksi` int(255) NOT NULL,
+  `idTransaksi` int(255) DEFAULT NULL,
   `idStorage` int(255) NOT NULL,
-  `quantity` int(255) NOT NULL,
-  `harga_satuan` int(255) NOT NULL
+  `idProduk` int(11) NOT NULL,
+  `quantity` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `detail`
+--
+
+INSERT INTO `detail` (`idTransaksi`, `idStorage`, `idProduk`, `quantity`) VALUES
+(7, 4, 1, 5),
+(9, 5, 4, 9),
+(11, 6, 4, 9),
+(13, 7, 2, 3),
+(14, 8, 3, 5),
+(15, 9, 7, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detailalamat`
+--
+
+CREATE TABLE `detailalamat` (
+  `idAlamat` int(11) NOT NULL,
+  `Alamat` varchar(255) NOT NULL,
+  `KodePos` varchar(6) NOT NULL,
+  `NomerTelepon` varchar(13) NOT NULL,
+  `Perusahaan` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `detailalamat`
+--
+
+INSERT INTO `detailalamat` (`idAlamat`, `Alamat`, `KodePos`, `NomerTelepon`, `Perusahaan`) VALUES
+(1, 'Jln Surya Sumantri', '40175', '082216648470', 'PT Sumber Makmur'),
+(2, 'Jln Surya Sumantri', '40002', '0823239321', 'PT Sumber Makmur'),
+(3, 'Jln Surya Sumantri', '40011', '082216648470', 'PT Sumber Jamur');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `produk`
+--
+
+CREATE TABLE `produk` (
+  `idProduk` int(11) NOT NULL,
+  `namaProduk` varchar(100) NOT NULL,
+  `lebar` varchar(5) NOT NULL,
+  `panjang` varchar(5) NOT NULL,
+  `hargaSatuan` decimal(19,0) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `produk`
+--
+
+INSERT INTO `produk` (`idProduk`, `namaProduk`, `lebar`, `panjang`, `hargaSatuan`) VALUES
+(1, 'Banner', '1.5', '1.5', '2000'),
+(2, 'Banner', '2', '2', '3000'),
+(3, 'Banner', '4', '4', '4000'),
+(4, 'Banner', '5', '5', '5000'),
+(5, 'Brosur 1 Sisi', '20', '10', '4000'),
+(6, 'Brosur 2 Sisi', '20', '10', '6000'),
+(7, 'Photo Frame', '3', '4', '3000'),
+(8, 'Photo ', '3', '4', '5000');
 
 -- --------------------------------------------------------
 
@@ -56,9 +126,22 @@ CREATE TABLE `detail` (
 
 CREATE TABLE `storage` (
   `idStorage` int(255) NOT NULL,
-  `file` mediumblob NOT NULL,
-  `idUser` int(255) NOT NULL
+  `filename` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `storage`
+--
+
+INSERT INTO `storage` (`idStorage`, `filename`) VALUES
+(2, 'Capture.PNG'),
+(3, '55rXIcs.jpg'),
+(4, '55rXIcs1.jpg'),
+(5, '55rXIcs2.jpg'),
+(6, '55rXIcs3.jpg'),
+(7, '55rXIcs4.jpg'),
+(8, 'This+this+feels+like+it+should+be+a+felony+_746fbbe4397005b8b3cbf50333b2c250.jpg'),
+(9, 'U5qP8TjJ_400x400.jpg');
 
 -- --------------------------------------------------------
 
@@ -69,11 +152,31 @@ CREATE TABLE `storage` (
 CREATE TABLE `transaksi` (
   `idTransaksi` int(255) NOT NULL,
   `idUser` int(255) NOT NULL,
-  `idDelivery` int(255) NOT NULL,
+  `idDelivery` int(255) DEFAULT NULL,
   `tagihan` int(255) NOT NULL,
-  `payment` varchar(20) NOT NULL,
-  `status` varchar(10) NOT NULL
+  `status` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+INSERT INTO `transaksi` (`idTransaksi`, `idUser`, `idDelivery`, `tagihan`, `status`) VALUES
+(1, 11, 1, 46000, 'Waiting Payment'),
+(2, 11, NULL, 10000, 'Waiting Payment'),
+(3, 11, NULL, 10000, 'Waiting Payment'),
+(4, 11, NULL, 10000, 'Waiting Payment'),
+(5, 11, NULL, 10000, 'Waiting Payment'),
+(6, 11, NULL, 10000, 'Waiting Payment'),
+(7, 11, NULL, 10000, 'Waiting Payment'),
+(8, 11, NULL, 45000, 'Waiting Payment'),
+(9, 11, NULL, 45000, 'Waiting Payment'),
+(10, 11, NULL, 45000, 'Waiting Payment'),
+(11, 11, NULL, 45000, 'Waiting Payment'),
+(12, 12, NULL, 9000, 'Waiting Payment'),
+(13, 12, NULL, 9000, 'Waiting Payment'),
+(14, 12, NULL, 20000, 'Waiting Payment'),
+(15, 11, NULL, 9000, 'Waiting Payment');
 
 -- --------------------------------------------------------
 
@@ -83,9 +186,10 @@ CREATE TABLE `transaksi` (
 
 CREATE TABLE `user` (
   `idUser` int(255) NOT NULL,
+  `email` varchar(40) NOT NULL,
   `username` varchar(16) NOT NULL,
   `password` char(10) NOT NULL,
-  `alamat` varchar(100) NOT NULL,
+  `alamat` int(11) DEFAULT NULL,
   `firstName` varchar(16) NOT NULL,
   `lastName` varchar(16) NOT NULL,
   `no_telp` varchar(12) NOT NULL
@@ -95,10 +199,13 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`idUser`, `username`, `password`, `alamat`, `firstName`, `lastName`, `no_telp`) VALUES
-(1, 'adityaray', '123456', 'jln. jalan', 'Aditya', 'Ray', '081323435231'),
-(6, 'adityarayy', '123456', 'jln. surya', 'Aditya', 'Ray', '081323435232'),
-(7, 'adityarey', '123456', 'jln. jurang', 'Aditya', 'Rayhan', '081323435233');
+INSERT INTO `user` (`idUser`, `email`, `username`, `password`, `alamat`, `firstName`, `lastName`, `no_telp`) VALUES
+(7, 'me@y.com', 'asdsadsad', 'e10adc3949', NULL, 'kasd', 'eqre', '082216648470'),
+(8, 'bftiga@gmail.com', 'adddddddd', 'e10adc3949', NULL, 'madityar', 'adit', '082216648470'),
+(9, 'bft@gmail.com', 'aditaea', 'e10adc3949', NULL, 'madityara', 'terq', '082216648470'),
+(10, 'm.sd@yaho.com', 'adityarayh', 'e10adc3949', NULL, 'adtiy', 'rwes', '082216648470'),
+(11, 'm.adityarayhan@yahool.co.id', 'adityar', '123456', 2, 'Muhammad', 'Rayhan', '+62822166484'),
+(12, 'm.a@yahoo.co.id', 'maditya', '123456', 1, 'aditya', 'rayhan', '082216648470');
 
 --
 -- Indexes for dumped tables
@@ -115,14 +222,26 @@ ALTER TABLE `delivery`
 --
 ALTER TABLE `detail`
   ADD KEY `idTransaksi` (`idTransaksi`),
-  ADD KEY `idStorage` (`idStorage`);
+  ADD KEY `idStorage` (`idStorage`),
+  ADD KEY `idProduk` (`idProduk`);
+
+--
+-- Indexes for table `detailalamat`
+--
+ALTER TABLE `detailalamat`
+  ADD PRIMARY KEY (`idAlamat`);
+
+--
+-- Indexes for table `produk`
+--
+ALTER TABLE `produk`
+  ADD PRIMARY KEY (`idProduk`);
 
 --
 -- Indexes for table `storage`
 --
 ALTER TABLE `storage`
-  ADD PRIMARY KEY (`idStorage`),
-  ADD KEY `idUser` (`idUser`);
+  ADD PRIMARY KEY (`idStorage`);
 
 --
 -- Indexes for table `transaksi`
@@ -136,7 +255,8 @@ ALTER TABLE `transaksi`
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`idUser`);
+  ADD PRIMARY KEY (`idUser`),
+  ADD KEY `alamat` (`alamat`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -146,25 +266,37 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `delivery`
 --
 ALTER TABLE `delivery`
-  MODIFY `idDelivery` int(255) NOT NULL AUTO_INCREMENT;
+  MODIFY `idDelivery` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `detailalamat`
+--
+ALTER TABLE `detailalamat`
+  MODIFY `idAlamat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `produk`
+--
+ALTER TABLE `produk`
+  MODIFY `idProduk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `storage`
 --
 ALTER TABLE `storage`
-  MODIFY `idStorage` int(255) NOT NULL AUTO_INCREMENT;
+  MODIFY `idStorage` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `transaksi`
 --
 ALTER TABLE `transaksi`
-  MODIFY `idTransaksi` int(255) NOT NULL AUTO_INCREMENT;
+  MODIFY `idTransaksi` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `idUser` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `idUser` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- Constraints for dumped tables
@@ -175,13 +307,8 @@ ALTER TABLE `user`
 --
 ALTER TABLE `detail`
   ADD CONSTRAINT `detail_fk1` FOREIGN KEY (`idStorage`) REFERENCES `storage` (`idStorage`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `detail_fk2` FOREIGN KEY (`idTransaksi`) REFERENCES `transaksi` (`idTransaksi`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Constraints for table `storage`
---
-ALTER TABLE `storage`
-  ADD CONSTRAINT `storage_fk1` FOREIGN KEY (`idUser`) REFERENCES `user` (`idUser`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `detail_fk2` FOREIGN KEY (`idTransaksi`) REFERENCES `transaksi` (`idTransaksi`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `detail_ibfk_1` FOREIGN KEY (`idProduk`) REFERENCES `produk` (`idProduk`);
 
 --
 -- Constraints for table `transaksi`
@@ -189,6 +316,12 @@ ALTER TABLE `storage`
 ALTER TABLE `transaksi`
   ADD CONSTRAINT `transaksi_fk1` FOREIGN KEY (`idDelivery`) REFERENCES `delivery` (`idDelivery`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `transaksi_fk2` FOREIGN KEY (`idUser`) REFERENCES `user` (`idUser`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`alamat`) REFERENCES `detailalamat` (`idAlamat`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
